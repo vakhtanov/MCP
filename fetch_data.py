b3946e76070e6f64a71b97163785d803c5c93fcb
Вахтанов Андрей Сергеевич <vakhtanov@gss.gascom.local>
2017-03-07 16:23:52 +0300

    good clear text

--- fetch_data.py
+++ fetch_data.py
@@ -2,7 +2,14 @@
 new program
 """
 
+# coding: utf-8 
+
+
 import urllib2
+#import html2text
+
+import lxml
+from lxml.html.clean import Cleaner
 
 from bs4 import BeautifulSoup
 
@@ -10,10 +17,30 @@ from bs4 import BeautifulSoup
 #http://www.gutenberg.org/files/135/135-h/135-h.htm
 
 def load_web_to_text(link='http://www.gutenberg.org/files/135/135-h/135-h.htm',file='out.txt'):
+	raw_file='raw_file.txt'
+
 	response=urllib2.urlopen(link)
 	html = response.read()
-	soup=BeautifulSoup(html,'html.parser')
-	plane_text=soup.get_text().encode('utf8')
+	with open(raw_file,'w+') as text_file: 
+		text_file.write(html)
+
+#	cleantext=cleaner.clean_html(lxml.html.parse(link))
+
+
+	soup_html=BeautifulSoup(html,'html.parser')
+	soup_html.script.clear()
+	soup_html.option.clear()
+	soup_html.p.clear()
+	
+
+#	soup_html=BeautifulSoup(html,'html5lib')
+#	txt=soup_html.find('div',{'class':'body'})
+#E	print txt
+	plane_text=soup_html.body.get_text().encode('utf8')
+#	plane_text=txt
+
+#	plane_text=cleantext
+
 	with open(file,'w+') as text_file: 
 		text_file.write(plane_text)
 	return plane_text
@@ -21,8 +48,10 @@ def load_web_to_text(link='http://www.gutenberg.org/files/135/135-h/135-h.htm',f
 def show_param(clear_text):
 #	if type(clear_text)=='str':
 	words = clear_text.split(' ')
+
 	text_param = (words[0] + '....' + words[len(words)-1]+' len: ' + str(len(words)))
-	return text_param
+
+	print text_param.decode('utf8').encode('cp866')
 
 #print (soup.get_text().encode('utf-8'))
 
--- run.py
+++ run.py
@@ -5,26 +5,36 @@ comment
 from markov_python.cc_markov import MarkovChain
 import fetch_data
 
-link='http://www.e-reading.club/bookreader.php/1020088/Fomina_-_Pritchi._Daosskie%2C_kitayskie%2C_dzenskie.html'
+link='http://www.e-reading.club/bookreader.php/1020088/Fomina_-_Pritchi._Daosskie%2C_kitayskie%2C_dzenskie.html' 
 #link='http://www.krotov.info/acts/01/joseph/filon_02.htm'
 
 vocabul=fetch_data.load_web_to_text(link,'voc.txt')
+print type(vocabul)
 
-print fetch_data.show_param(str(vocabul))
+fetch_data.show_param(str(vocabul))
 
+out_fileS='out_text_string.txt'
 out_file='out_text.txt'
 
 text_object=MarkovChain(3)
 text_object.add_file('voc.txt')
 out_text = text_object.generate_text(200)
-for i in out_text: 	print type(i),i.decode('cp866')
-out_text_j = ' '.join(out_text)
-with open(out_file,'w+') as ofile:
-	for i in out_text: ofile.write(i+'\n')
 
+
+for i in out_text:
+	try:
+	 	print type(i),i.decode('utf8').encode('cp866')
+	except:
+		print 'Error'
+
+out_text_j = ' '.join(out_text)
+#with open(out_fileS,'w+') as ofile: 
+#	for i in out_text: ofile.write(i+'\n')
+with open(out_file,'w+') as ofile: 
+	for i in out_text: ofile.write(i+' ')
 
 #print out_text.decode('utf-8')
 #print out_text_j.decode('utf-8')
 
-print 'Error'
+
 
